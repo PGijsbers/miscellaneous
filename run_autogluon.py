9ce5b0af5096846b493a75577a3e95278924870a
PGijsbers <p.gijsbers@tue.nl>
2025-06-12 20:15:45 +0200

    Make workflow configurable

--- run_autogluon.py
+++ run_autogluon.py
@@ -1,14 +1,17 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
 import openml
+import sys
+
+_, time, task = sys.argv
 
 print("Downloading OpenML task")
-task = openml.tasks.get_task(363580)
+task = openml.tasks.get_task(int(task))
 dataset = task.get_dataset()
 data, *_ = dataset.get_data()
 
 print("Training AutoGluon")
 ag_data = TabularDataset(data)
-predictor = TabularPredictor(label='Class').fit(train_data=ag_data, time_limit=60)
+predictor = TabularPredictor(label='Class').fit(train_data=ag_data, time_limit=int(time))
 
 print("Training finished successfully")
 predictions = predictor.predict(data)

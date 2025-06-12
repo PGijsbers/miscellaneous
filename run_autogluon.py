a24d432d6cab38cf99109a9bada0a38393f1791c
PGijsbers <p.gijsbers@tue.nl>
2025-06-12 20:00:43 +0200

    Update script to run AG

--- run_autogluon.py
+++ run_autogluon.py
@@ -1,10 +1,12 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
 import openml
 
+print("Downloading OpenML task")
 task = openml.tasks.get_task(363580)
 dataset = task.get_dataset()
 data, *_ = dataset.get_data()
 
+print("Training AutoGluon")
 ag_data = TabularDataset(data)
 predictor = TabularPredictor(label='Class').fit(train_data=ag_data, time_limit=60)
 

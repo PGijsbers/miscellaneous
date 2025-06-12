a44ec5bed845e51ceb2649f3f2171bd32e2cdf1c
PGijsbers <p.gijsbers@tue.nl>
2025-06-12 21:13:19 +0200

    Provide test data to autogluon

--- run_autogluon.py
+++ run_autogluon.py
@@ -5,16 +5,25 @@ import sys
 if len(sys.argv) == 3:
     _, time, task = sys.argv
 else:
-    time, task = 60, 363580
+time, task = 60, 363580
 
 print("Downloading OpenML task")
 task = openml.tasks.get_task(int(task))
 dataset = task.get_dataset()
 data, *_ = dataset.get_data()
 
+train_ind, test_ind = task.get_train_test_split_indices()
+train_data = TabularDataset(data.iloc[train_ind,:])
+test_data = TabularDataset(data.iloc[test_ind,:])
+
 print("Training AutoGluon")
-ag_data = TabularDataset(data)
-predictor = TabularPredictor(label='Class', verbosity=4).fit(train_data=ag_data, time_limit=int(time))
+predictor = TabularPredictor(
+    label='Class', verbosity=4
+).fit(
+    train_data=train_data,
+    test_data=test_data,
+    time_limit=int(time)
+)
 
 print("Training finished successfully")
 predictions = predictor.predict(data)
